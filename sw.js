d7341bcffbaf913c433ba0c782a52332f405ad93
Bolgino <marcobolge@gmail.com>
2026-01-20 15:27:13 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'azimut-arch-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  // Aggiungi qui eventuali immagini locali se ne hai, es: './icon-192.png'
+];
+
+// Installazione del Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Strategia: Network First (Cerca online, se non va usa la cache)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// Attivazione e pulizia vecchie cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
