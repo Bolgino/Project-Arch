1d6a1c78300a7cf76104558ec8acf4ad26bffb1e
Bolgino <marcobolge@gmail.com>
2026-01-31 19:10:56 +0100

    Update cambusa.js

--- cambusa.js
+++ cambusa.js
@@ -4,7 +4,8 @@
 const CONFIG = {
     url: "https://jmildwxjaviqkrkhjzhl.supabase.co", 
     key: "sb_publishable_PwYQxh8l7HLR49EC_wHa7A_gppKi_FS", 
-    adminEmail: "marcobolge@gmail.com"
+    adminEmail: "marcobolge@gmail.com",
+    bucket: "immagini-oggetti"
 };
 
 const _sb = supabase.createClient(CONFIG.url, CONFIG.key);
